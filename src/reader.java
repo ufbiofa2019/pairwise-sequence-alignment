f3bc00ea03bffb1cebeacd41d114b5c1ca9e1cee
Phelan Vo <s24471468@gmail.com>
2019-11-01 20:54:35 -0400

    Added memory and runtime calculations

--- src/reader.java
+++ src/reader.java
@@ -3,32 +3,49 @@ import java.util.*;
 public class Reader {
 
     public static void main(String[] args) {
-	    System.out.println("Hello world");
-	    String randomSeq = generateSeq(1000);
+        Runtime run = Runtime.getRuntime();
+        //calculate starting memory use and start time
+        long startmem = run.totalMemory()- run.freeMemory();
+        long starttime = System.nanoTime();
+	    String randomSeq = generateSeq(600000000);
+        //calculate ending memory use and end time
+        long endtime = System.nanoTime();
+        long endmem = run.totalMemory()- run.freeMemory();
+
+        //calculate total memory/runtime
+        long duration = endtime - starttime;
+        long usedmem = endmem - startmem;
+
+        System.out.println("RUNTIME IN NANOSEC: " + duration);
+        System.out.println("MEMORY USE IN GB: " + (double)usedmem/1000000000);
+        long seconds = duration/1000000000;
+        long minutes = seconds/60;
+        long hours = minutes/60;
+        System.out.println("HOURS: " + hours + "\nMINUTES: " + minutes + "\nSECONDS: " + seconds);
     }
 
-    public static String generateSeq(int length) {
-        String str = "";
+    /*Takes in an integer n and generates
+    * a random sequence string of A,T,C,G of length n*/
+    private static String generateSeq(int length) {
+        StringBuilder str = new StringBuilder();
         int max =4,min=1;
         int range = max - min + 1;
 
         for (int i = 0; i < length; i++) {
             int rand = (int)(Math.random() * range) + min;
             if (rand == 1) {
-                str += "A";
+                str.append("A");
             }
             if (rand == 2) {
-                str += "T";
+                str.append("T");
             }
             if (rand == 3) {
-                str += "C";
+                str.append("C");
             }
             if (rand == 4) {
-                str += "G";
+                str.append("G");
             }
-            //System.out.println(rand);
         }
-        System.out.println(str);
-        return str;
+        return str.toString();
     }
 }
