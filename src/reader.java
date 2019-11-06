2617c28291aae5c1f90aff29ae936e4bdaf02e3a
Andrew Rocha <ahrocha96@gmail.com>
2019-11-05 19:40:53 -0500

    Creates dp matrix and traces through for alignment

--- src/reader.java
+++ src/reader.java
@@ -1,8 +1,19 @@
 import java.util.*;
 
+import java.io.*; 
+import java.util.*; 
+import java.lang.*; 
+
+
 public class Reader {
 
     public static void main(String[] args) {
+    	
+ 	    String gene1 = "ATCAGAGTC"; 
+ 	    String gene2 = "TTCAGTC"; 
+ 	      
+ 	    getMaxScore(gene1, gene2); 
+ 	    
         Runtime run = Runtime.getRuntime();
         //calculate starting memory use and start time
         long startmem = run.totalMemory()- run.freeMemory();
@@ -48,4 +59,147 @@ public class Reader {
         }
         return str.toString();
     }
+    
+ 	public static void getMaxScore(String gene1, String gene2) { 
+ 	   
+ 		int mismatchPenalty = -1;
+ 		int gapPenalty = -2;
+ 		int matchScore = 2;
+ 	    int gene1Length = gene1.length(); 
+ 	    int gene2Length = gene2.length(); 
+ 	    int maxLength = gene2Length + gene1Length; // maximum possible length 
+ 	    
+ 	      
+ 	    int scoreMatrix[][] = new int[maxLength + 1][maxLength + 1]; 
+ 	      
+ 	    for (int[] filler : scoreMatrix) {
+ 	    	Arrays.fill(filler, 0);
+ 	    } 
+ 	    
+ 	    for (int i = 0; i <= (maxLength); i++) 
+ 	    { 
+ 	        scoreMatrix[i][0] = i * gapPenalty; 
+ 	        scoreMatrix[0][i] = i * gapPenalty; 
+ 	    }     	  
+
+ 	    //Fill the matrix
+ 	    for (int i = 1; i <= gene1Length; i++) { 
+ 	        for (int j = 1; j <= gene2Length; j++) { 
+ 	        	
+ 	        	int largest = - 2147483648;
+ 	        	
+ 	            if (gene1.charAt(i - 1) == gene2.charAt(j - 1)) { 
+ 	            	largest = Math.max(scoreMatrix[i][j - 1] + gapPenalty , scoreMatrix[i - 1][j] + gapPenalty);
+ 	            	largest = Math.max(largest, scoreMatrix[i - 1][j - 1] + matchScore); 
+ 	            } 
+ 	            
+ 	            else{ 
+ 	            	largest = Math.max(scoreMatrix[i][j - 1] + gapPenalty , scoreMatrix[i - 1][j] + gapPenalty);
+ 	            	largest = Math.max(largest , scoreMatrix[i - 1][j - 1] + mismatchPenalty); 
+ 	                
+ 	            } 
+ 	            
+                 scoreMatrix[i][j] = largest;
+ 	        } 
+ 	    } 
+ 	    
+ 	    //Display filled matrix
+ 	    System.out.println("\n\n");
+ 	    for (int row = 0; row < scoreMatrix.length; row++) {
+ 	    	  
+             for (int  column= 0; column < scoreMatrix[row].length; column++) {
+             	System.out.print(scoreMatrix[row][column] + " ");
+             }
+             
+             System.out.println("\n");
+             
+         }
+ 	    
+ 	  	    
+ 	    int i = gene1Length;
+ 	    int j = gene2Length;
+ 	    int gene1Position = maxLength; 
+ 	    int gene2Position = maxLength;
+ 	    char gene1Alignment[] = new char[maxLength + 1];  
+ 	    char gene2Alignment[] = new char[maxLength + 1]; 
+ 	    
+ 	      
+ 	    //Backtracks through the matrix to find the optimal solution
+ 	    while ( !(i == 0 || j == 0)) 
+ 	    { 
+ 	        if (gene1.charAt(i - 1) == gene2.charAt(j - 1)) { 
+ 	            gene1Alignment[gene1Position--] = gene1.charAt(i - 1); 
+ 	            gene2Alignment[gene2Position--] = gene2.charAt(j - 1); 
+ 	            i--; 
+ 	            j--; 
+ 	        } 
+ 	        
+ 	        else if (scoreMatrix[i - 1][j - 1] + mismatchPenalty == scoreMatrix[i][j]) { 
+ 	            gene1Alignment[gene1Position--] = gene1.charAt(i - 1); 
+ 	            gene2Alignment[gene2Position--] = gene2.charAt(j - 1); 
+ 	            i--;
+ 	            j--; 
+ 	        } 
+ 	        
+ 	        else if (scoreMatrix[i - 1][j] + gapPenalty == scoreMatrix[i][j]) { 
+ 	            gene1Alignment[gene1Position--] = gene1.charAt(i - 1); 
+ 	            gene2Alignment[gene2Position--] = '_'; 
+ 	            i--; 
+ 	        } 
+ 	        
+ 	        else if (scoreMatrix[i][j - 1] + gapPenalty == scoreMatrix[i][j]) { 
+ 	            gene1Alignment[gene1Position--] = '_'; 
+ 	            gene2Alignment[gene2Position--] = gene2.charAt(j - 1); 
+ 	            j--; 
+ 	        } 
+ 	    } 
+ 	    
+ 	    //Fills out the rest of alignment
+ 	    while (gene1Position > 0) 
+ 	    { 
+ 	        if (i > 0) {
+ 	        	gene1Alignment[gene1Position--] = gene1.charAt(--i); 
+ 	        }
+ 	        
+ 	        else {
+ 	        	gene1Alignment[gene1Position--] = '_'; 
+ 	        }
+ 	    } 
+ 	    
+ 	    while (gene2Position > 0) 
+ 	    { 
+ 	        if (j > 0) {
+ 	        	gene2Alignment[gene2Position--] = gene2.charAt(--j); 
+ 	        }
+ 	        else {
+ 	        	gene2Alignment[gene2Position--] = '_'; 
+ 	        }
+ 	    } 
+ 	  
+ 	    
+ 	    int startIndexOfAlignment = 1; 
+ 	    
+ 	    //Finds relevant part of matrix
+ 	    for (i = maxLength; i >= 1; i--) { 
+ 	        if (gene2Alignment[i] == '_'  &&  gene1Alignment[i] == '_') { 
+ 	            startIndexOfAlignment = i + 1; 
+ 	            break; 
+ 	        } 
+ 	    } 
+ 	  
+ 	    /*PRINTED SOLUTION*/ 
+ 	    System.out.print("Maximum score: " + scoreMatrix[gene1Length][gene2Length] + "\n"); 
+ 	    System.out.println("Aligned genes: "); 
+ 	    for (i = startIndexOfAlignment; i <= maxLength; i++) { 
+ 	        System.out.print(gene1Alignment[i]); 
+ 	    } 
+ 	    
+ 	    System.out.print("\n"); 
+ 	    for (i = startIndexOfAlignment; i <= maxLength; i++) { 
+ 	        System.out.print(gene2Alignment[i]); 
+ 	    } 
+ 	    
+ 	    return; 
+ 	} 
+ 	  
 }
